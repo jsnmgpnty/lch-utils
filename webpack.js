0c6cdba264caa938338867d31cf422f7006748da
Jason <jsnmgpnty@gmail.com>
2022-06-06 12:04:27 +0800

    test

--- webpack.js
+++ webpack.js
@@ -6,6 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     library: 'lchColorUtils',
-    libraryTarget: 'umd',
+    libraryTarget: 'module',
+    module: true,
   },
 };
